4fb712090880a9739dc148787b2edda70ab5eb51
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-06-13 00:10:59 +0530

    index

--- server/index.js
+++ server/index.js
@@ -1,35 +1,44 @@
 const express = require("express");
-const { ApoloServer } = require("@apollo/server");
-const { expressMiddleware } = require("body-parser");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
-async function startServer(){
-    const app= express();
-    const server = new ApoloServer({
-        typeDefs:`
-        type Todo{
-            id:ID!
-            tittle:string!
+async function startServer() {
+    const app = express();
+
+    const server = new ApolloServer({
+        typeDefs: `
+        type Todo {
+            id: ID!
+            title: String!
             completed: Boolean
         }
             
-        type Query{
-            getTodos:[Todo]
+        type Query {
+            getTodos: [Todo]
         }
         `,
-        resolvers: {},
+        resolvers: {
+            Query: {
+                getTodos: () => [
+                    { id: "1", title: "First Todo", completed: false },
+                    { id: "2", title: "Second Todo", completed: true }
+                ],
+            },
+        },
     });
-
-    app.use(bodyParer.json());
-    app.use(cors());
 
     await server.start();
 
+    app.use(bodyParser.json());
+    app.use(cors());
+
     app.use("/graphql", expressMiddleware(server));
 
-    app.listen(8000, ()=>{
-        console.log('Server Started at PORT 8000');
-    })
+    app.listen(8000, () => {
+        console.log('Server started at PORT 8000');
+    });
 }
 
 startServer();
